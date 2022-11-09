9ac41e1d839ddb353a2a8b3e5fb4d92a6d092f6c
mcarri355 <carrillomatisa@gmail.com>
2022-11-08 22:18:25 -0700

    updated css
    changed colors and size of x's and o's

--- ticTacToe.js
+++ ticTacToe.js
@@ -25,119 +25,167 @@ function playerTurn(){
    }
 };
 
+//Function that affects top left box when clicked
 function box1Click(){
+    //When player 1 clicks
     if(turn%2 == 0 && document.getElementById("box1").innerHTML == "" && win == false){
         turn++;
         document.getElementById("box1").innerHTML = "X";
+        document.getElementById("box1").style.color = "blue";
+        document.getElementById("box1").style.fontSize = "150px";
         box1 = "X";
     }
+
+    //When player 2 clicks
     if(turn%2 ==1 && document.getElementById("box1").innerHTML == "" && win == false){
         turn++
         document.getElementById("box1").innerHTML = "O";
+        document.getElementById("box1").style.color = "red";
+        document.getElementById("box1").style.fontSize = "150px";
         box1 = "O";
     }
 };
 
+//Function that affects top middle box when clicked
 function box2Click(){
     if(turn%2 == 0 && document.getElementById("box2").innerHTML == "" && win == false){
         turn++;
         document.getElementById("box2").innerHTML = "X";
+        document.getElementById("box2").style.color = "blue";
+        document.getElementById("box2").style.fontSize = "150px";
         box2 = "X";
     }
     if(turn%2 ==1 && document.getElementById("box2").innerHTML == "" && win == false){
         turn++
         document.getElementById("box2").innerHTML = "O";
+        document.getElementById("box2").style.color = "red";
+        document.getElementById("box2").style.fontSize = "150px";
         box2 = "O";
     }
 };
 
+//Function that affects top right box when clicked
 function box3Click(){
     if(turn%2 == 0 && document.getElementById("box3").innerHTML == "" && win == false){
         turn++;
         document.getElementById("box3").innerHTML = "X";
+        document.getElementById("box3").style.color = "blue";
+        document.getElementById("box3").style.fontSize = "150px";
         box3 = "X";
     }
     if(turn%2 ==1 && document.getElementById("box3").innerHTML == "" && win == false){
         turn++
         document.getElementById("box3").innerHTML = "O";
+        document.getElementById("box3").style.color = "red";
+        document.getElementById("box3").style.fontSize = "150px";
         box3 = "O";
     }
 };
 
+//Function that affects middle left box when clicked
 function box4Click(){
     if(turn%2 == 0 && document.getElementById("box4").innerHTML == "" && win == false){
         turn++;
         document.getElementById("box4").innerHTML = "X";
+        document.getElementById("box4").style.color = "blue";
+        document.getElementById("box4").style.fontSize = "150px";
         box4 = "X";
     }
     if(turn%2 == 1 && document.getElementById("box4").innerHTML == "" && win == false){
         turn++
         document.getElementById("box4").innerHTML = "O";
+        document.getElementById("box4").style.color = "red";
+        document.getElementById("box4").style.fontSize = "150px";
         box4 = "O";
     }
 };
 
+//Function that affects middle box when clicked
 function box5Click(){
     if(turn%2 == 0 && document.getElementById("box5").innerHTML == "" && win == false){
         turn++;
         document.getElementById("box5").innerHTML = "X";
+        document.getElementById("box5").style.color = "blue";
+        document.getElementById("box5").style.fontSize = "150px";
         box5 = "X";
     }
     if(turn%2 ==1 && document.getElementById("box5").innerHTML == "" && win == false){
         turn++
         document.getElementById("box5").innerHTML = "O";
+        document.getElementById("box5").style.color = "red";
+        document.getElementById("box5").style.fontSize = "150px";
         box5 = "O";
     }
 };
 
+//Function that affects middle right box when clicked
 function box6Click(){
     if(turn%2 == 0 && document.getElementById("box6").innerHTML == "" && win == false){
         turn++;
         document.getElementById("box6").innerHTML = "X";
+        document.getElementById("box6").style.color = "blue";
+        document.getElementById("box6").style.fontSize = "150px";
         box6 = "X";
     }
     if(turn%2 ==1 && document.getElementById("box6").innerHTML == "" && win == false){
         turn++
         document.getElementById("box6").innerHTML = "O";
+        document.getElementById("box6").style.color = "red";
+        document.getElementById("box6").style.fontSize = "150px";
         box6 = "O";
     }
 };
 
+//Function that affects bottom left box when clicked
 function box7Click(){
     if(turn%2 == 0 && document.getElementById("box7").innerHTML == "" && win == false){
         turn++;
         document.getElementById("box7").innerHTML = "X";
+        document.getElementById("box7").style.color = "blue";
+        document.getElementById("box7").style.fontSize = "150px";
         box7 = "X";
     }
     if(turn%2 ==1 && document.getElementById("box7").innerHTML == "" && win == false){
         turn++
         document.getElementById("box7").innerHTML = "O";
+        document.getElementById("box7").style.color = "red";
+        document.getElementById("box7").style.fontSize = "150px";
         box7 = "O";
     }
 };
 
+//Function that affects bottom middle box when clicked
 function box8Click(){
     if(turn%2 == 0 && document.getElementById("box8").innerHTML == "" && win == false){
         turn++;
         document.getElementById("box8").innerHTML = "X";
+        document.getElementById("box8").style.color = "blue";
+        document.getElementById("box8").style.fontSize = "150px";
         box8 = "X";
     }
     if(turn%2 ==1 && document.getElementById("box8").innerHTML == "" && win == false){
         turn++
         document.getElementById("box8").innerHTML = "O";
+        document.getElementById("box8").style.color = "red";
+        document.getElementById("box8").style.fontSize = "150px";
         box8 = "O";
     }
 };
 
+//Function that affects bottom right box when clicked
 function box9Click(){
     if(turn%2 == 0 && document.getElementById("box9").innerHTML == "" && win == false){
         turn++;
         document.getElementById("box9").innerHTML = "X";
+        document.getElementById("box9").style.color = "blue";
+        document.getElementById("box9").style.fontSize = "150px";
         box9 = "X";
     }
     if(turn%2 ==1 && document.getElementById("box9").innerHTML == "" && win == false){
         turn++
         document.getElementById("box9").innerHTML = "O";
+        document.getElementById("box9").style.color = "red";
+        document.getElementById("box9").style.fontSize = "150px";
         box9 = "O";
     }
 };
@@ -147,6 +195,7 @@ function winner(){
     //If X Wins
     if(box1 == "X" && box2 == "X" && box3 == "X" && win == false) {
         document.getElementById('winner').innerHTML = 'P1 Wins!';
+        document.getElementById('winner').style.color = 'blue'
         document.getElementById('box1').style.backgroundColor = 'gold'
         document.getElementById('box2').style.backgroundColor = 'gold'
         document.getElementById('box3').style.backgroundColor = 'gold'
@@ -156,6 +205,7 @@ function winner(){
     };
     if(box4 == "X" && box5 == "X" && box6 == "X" && win == false) {
         document.getElementById('winner').innerHTML = 'P1 Wins!';
+        document.getElementById('winner').style.color = 'blue'
         document.getElementById('box4').style.backgroundColor = 'gold'
         document.getElementById('box5').style.backgroundColor = 'gold'
         document.getElementById('box6').style.backgroundColor = 'gold'
@@ -165,6 +215,7 @@ function winner(){
     };
     if(box7 == "X" && box8 == "X" && box9 == "X" && win == false) {
         document.getElementById('winner').innerHTML = 'P1 Wins!';
+        document.getElementById('winner').style.color = 'blue'
         document.getElementById('box7').style.backgroundColor = 'gold'
         document.getElementById('box8').style.backgroundColor = 'gold'
         document.getElementById('box9').style.backgroundColor = 'gold'
@@ -174,6 +225,7 @@ function winner(){
     };
     if(box1 == "X" && box4 == "X" && box7 == "X" && win == false) {
         document.getElementById('winner').innerHTML = 'P1 Wins!';
+        document.getElementById('winner').style.color = 'blue'
         document.getElementById('box1').style.backgroundColor = 'gold'
         document.getElementById('box4').style.backgroundColor = 'gold'
         document.getElementById('box7').style.backgroundColor = 'gold'
@@ -183,6 +235,7 @@ function winner(){
     };
     if(box2 == "X" && box5 == "X" && box8 == "X" && win == false) {
         document.getElementById('winner').innerHTML = 'P1 Wins!';
+        document.getElementById('winner').style.color = 'blue'
         document.getElementById('box2').style.backgroundColor = 'gold'
         document.getElementById('box5').style.backgroundColor = 'gold'
         document.getElementById('box8').style.backgroundColor = 'gold'
@@ -192,6 +245,7 @@ function winner(){
     }; 
     if(box3 == "X" && box6 == "X" && box9 == "X" && win == false) {
         document.getElementById('winner').innerHTML = 'P1 Wins!';
+        document.getElementById('winner').style.color = 'blue'
         document.getElementById('box3').style.backgroundColor = 'gold'
         document.getElementById('box6').style.backgroundColor = 'gold'
         document.getElementById('box9').style.backgroundColor = 'gold'
@@ -201,6 +255,7 @@ function winner(){
     }; 
     if(box1 == "X" && box5 == "X" && box9 == "X" && win == false) {
         document.getElementById('winner').innerHTML = 'P1 Wins!';
+        document.getElementById('winner').style.color = 'blue'
         document.getElementById('box1').style.backgroundColor = 'gold'
         document.getElementById('box5').style.backgroundColor = 'gold'
         document.getElementById('box9').style.backgroundColor = 'gold'
@@ -210,6 +265,7 @@ function winner(){
     };
     if(box3 == "X" && box5 == "X" && box7 == "X" && win == false) {
         document.getElementById('winner').innerHTML = 'P1 Wins!';
+        document.getElementById('winner').style.color = 'blue'
         document.getElementById('box3').style.backgroundColor = 'gold'
         document.getElementById('box5').style.backgroundColor = 'gold'
         document.getElementById('box7').style.backgroundColor = 'gold'
@@ -221,6 +277,7 @@ function winner(){
     //If O Wins
     if(box1 == "O" && box2 == "O" && box3 == "O" && win == false) {
         document.getElementById('winner').innerHTML = 'P2 Wins!';
+        document.getElementById('winner').style.color = 'red'
         document.getElementById('box1').style.backgroundColor = 'gold'
         document.getElementById('box2').style.backgroundColor = 'gold'
         document.getElementById('box3').style.backgroundColor = 'gold'
@@ -230,6 +287,7 @@ function winner(){
     };
     if(box4 == "O" && box5 == "O" && box6 == "O" && win == false) {
         document.getElementById('winner').innerHTML = 'P2 Wins!';
+        document.getElementById('winner').style.color = 'red'
         document.getElementById('box4').style.backgroundColor = 'gold'
         document.getElementById('box5').style.backgroundColor = 'gold'
         document.getElementById('box6').style.backgroundColor = 'gold'
@@ -239,6 +297,7 @@ function winner(){
     };
     if(box7 == "O" && box8 == "O" && box9 == "O" && win == false) {
         document.getElementById('winner').innerHTML = 'P2 Wins!';
+        document.getElementById('winner').style.color = 'red'
         document.getElementById('box7').style.backgroundColor = 'gold'
         document.getElementById('box8').style.backgroundColor = 'gold'
         document.getElementById('box9').style.backgroundColor = 'gold'
@@ -248,6 +307,7 @@ function winner(){
     };
     if(box1 == "O" && box4 == "O" && box7 == "O" && win == false){
         document.getElementById('winner').innerHTML = 'P2 Wins!';
+        document.getElementById('winner').style.color = 'red'
         document.getElementById('box1').style.backgroundColor = 'gold'
         document.getElementById('box4').style.backgroundColor = 'gold'
         document.getElementById('box7').style.backgroundColor = 'gold'
@@ -257,6 +317,7 @@ function winner(){
     };
     if(box2 == "O" && box5 == "O" && box8 == "O" && win == false) {
         document.getElementById('winner').innerHTML = 'P2 Wins!';
+        document.getElementById('winner').style.color = 'red'
         document.getElementById('box2').style.backgroundColor = 'gold'
         document.getElementById('box5').style.backgroundColor = 'gold'
         document.getElementById('box8').style.backgroundColor = 'gold'
@@ -266,6 +327,7 @@ function winner(){
     }; 
     if(box3 == "O" && box6 == "O" && box9 == "O" && win == false) {
         document.getElementById('winner').innerHTML = 'P2 Wins!';
+        document.getElementById('winner').style.color = 'red'
         document.getElementById('box3').style.backgroundColor = 'gold'
         document.getElementById('box6').style.backgroundColor = 'gold'
         document.getElementById('box9').style.backgroundColor = 'gold'
@@ -275,6 +337,7 @@ function winner(){
     };
     if(box1 == "O" && box5 == "O" && box9 == "O" && win == false) {
         document.getElementById('winner').innerHTML = 'P2 Wins!';
+        document.getElementById('winner').style.color = 'red'
         document.getElementById('box1').style.backgroundColor = 'gold'
         document.getElementById('box5').style.backgroundColor = 'gold'
         document.getElementById('box9').style.backgroundColor = 'gold'
@@ -284,6 +347,7 @@ function winner(){
     };
     if(box3 == "O" && box5 == "O" && box7 == "O" && win == false) {
         document.getElementById('winner').innerHTML = 'P2 Wins!';
+        document.getElementById('winner').style.color = 'red'
         document.getElementById('box3').style.backgroundColor = 'gold'
         document.getElementById('box5').style.backgroundColor = 'gold'
         document.getElementById('box7').style.backgroundColor = 'gold'
